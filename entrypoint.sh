07da9bc90882ac7ad8d1f370cd932263eec28fd8
Sameer Naik <sameer@damagehead.com>
2015-07-21 19:12:42 +0530

    entrypoint.sh: use `which`

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,7 +24,7 @@ chown -R ${REDIS_USER}:${REDIS_USER} ${REDIS_DATA_DIR}
 if [[ ${1:0:1} = '-' ]]; then
   EXTRA_ARGS="$@"
   set --
-elif [[ ${1} == redis-server || ${1} == /usr/bin/redis-server ]]; then
+elif [[ ${1} == redis-server || ${1} == $(which redis-server) ]]; then
   EXTRA_ARGS="${@:2}"
   set --
 fi
@@ -32,7 +32,7 @@ fi
 # default behaviour is to launch redis-server
 if [[ -z ${1} ]]; then
   echo "Starting redis-server..."
-  exec start-stop-daemon --start --chuid ${REDIS_USER}:${REDIS_USER} --exec /usr/bin/redis-server -- \
+  exec start-stop-daemon --start --chuid ${REDIS_USER}:${REDIS_USER} --exec $(which redis-server) -- \
     /etc/redis/redis.conf ${REDIS_PASSWORD:+--requirepass $REDIS_PASSWORD} ${EXTRA_ARGS}
 else
   exec "$@"
